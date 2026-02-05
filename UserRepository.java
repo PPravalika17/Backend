2a2eff795bff044dc5b53d1fdbde086d9c9fa2dc
saathvika04 <saathvika728@gmail.com>
2026-02-05 05:40:37 +0000

    Add files via upload

---
+++ UserRepository.java
@@ -0,0 +1,12 @@
+package com.stockmarket.repository;
+
+import com.stockmarket.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // This allows Spring Security to find a user by their username during login
+    Optional<User> findByUsername(String username);
+}
